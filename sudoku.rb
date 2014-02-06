0eed996a4cdd9a5058859cf09d18be7f76f1d289
kalle malmiharju <malmiharju@btinternet.com>
2014-02-06 17:22:37 +0000

    problems solved

--- sudoku.rb
+++ sudoku.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+require 'sinatra/partial'
+require 'rack-flash'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
+require_relative './helpers/application'
 
 enable :sessions
+set :partial_template_engine, :erb
+use Rack::Flash
 
 def random_sudoku
     # we're using 9 numbers, 1 to 9, and 72 zeros as an input
@@ -58,6 +63,7 @@ post '/' do
   session[:check_solution] = true
   redirect to("/")
 end
+
 
 
 def box_order_to_row_order(cells)
@@ -123,25 +129,5 @@ def prepare_to_check_solution
   session[:check_solution] = nil
 end
 
-helpers do
 
-  def cell_value(value)
-    value.to_i == 0 ? '' : value
-  end    
-
-  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-    must_be_guessed = puzzle_value == 0
-	tried_to_guess = current_solution_value.to_i != 0
-    guessed_incorrectly = current_solution_value != solution_value
-
-    if solution_to_check && 
-        must_be_guessed && 
-        tried_to_guess && 
-        guessed_incorrectly
-      'incorrect'
-    elsif !must_be_guessed
-      'value-provided'
-    end
-  end
-end
 
